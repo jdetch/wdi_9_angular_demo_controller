b8629f79b3b944ddc51bb91371bfecd399b64ad5
Jodi Detch <jodidetch@gmail.com>
2014-08-06 16:29:29 -0400

    IIFE example

---
+++ js/iife_example.js
@@ -0,0 +1,9 @@
+//what JS scope is this function defined in?
+// The global scope
+
+// Polluting the Global Namespace
+// If we had a JS library that ALSO had a variable or function of this name,
+// 'say', it would cause a naming conflict
+function say(){
+  alert('Say Hello');
+}
